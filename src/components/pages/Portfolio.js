c60de7aaac54c6c9b842792c416c8151c6fb45c1
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-10-28 18:11:25 -0400

    portfolio changes

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,21 +4,22 @@ import { MDBRipple } from 'mdb-react-ui-kit';
 const Portfolio = () => {
   return (
     <div className="container">
-      <h1>Portfolio</h1>
-      <section>
-      <MDBRipple
-      className='bg-image hover-overlay shadow-1-strong rounded'
-      rippleTag='div'
-      rippleColor='light'
-    >
-      <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp' className='w-100'alt="..." />
-        <div className='mask' style={{ backgroundColor: 'rgba(, 251, 251, .20)' }}></div>
-      </a>
-    </MDBRipple>
+    <h1>Portfolio</h1>
+    <section>
+    <MDBRipple
+    className='bg-image hover-overlay shadow-1-strong rounded'
+    rippleTag='div'
+    rippleColor='light'
+  >
+    <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp'  width={400} height={400} alt="..." />
+      <div className='mask' style={{ backgroundColor: 'rgba(, 251, 251, .20)' }}></div>
+    </a>
+  </MDBRipple>
 
-      </section>
-    </div>
-  );
+    </section>
+  </div>
+  )
+  
 };
 
 export default Portfolio;
