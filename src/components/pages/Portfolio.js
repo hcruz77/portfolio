f7c346d57615defcc836d0701a037c0cba6de030
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-10-28 23:44:09 -0400

    forgot to save before last commit

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -65,7 +65,7 @@ const Portfolio = () => {
               <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp' width={400} height={400} className="card-img" alt="..." />
               </a>
             </MDBRipple>
-            <div class="card-body pb -6">
+            <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
