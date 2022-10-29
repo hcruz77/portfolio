8d0483d0a0e593ce4f9ed65f8378172be6a685f1
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-10-28 23:43:14 -0400

    added cards and linkable photos to portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,25 +1,116 @@
 import React from "react";
 import { MDBRipple } from 'mdb-react-ui-kit';
 
+
 const Portfolio = () => {
   return (
     <div className="container">
-    <h1>Portfolio</h1>
-    <section>
-    <MDBRipple
-    className='bg-image hover-overlay shadow-1-strong rounded'
-    rippleTag='div'
-    rippleColor='light'
-  >
-    <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp'  width={400} height={400} alt="..." />
-      <div className='mask' style={{ backgroundColor: 'rgba(, 251, 251, .20)' }}></div>
-    </a>
-  </MDBRipple>
+      <h1>Portfolio</h1>
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col">
+          <div class="card">
+            <MDBRipple
+              className='bg-image hover-overlay shadow-1-strong rounded'
+              rippleTag='div'
+              rippleColor='light'
+            >
+              <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp' width={400} height={400} className="card-img" alt="..." />
+              </a>
+            </MDBRipple>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <MDBRipple
+              className='bg-image hover-overlay shadow-1-strong rounded'
+              rippleTag='div'
+              rippleColor='light'
+            >
+              <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp' width={400} height={400} className="card-img" alt="..." />
+              </a>
+            </MDBRipple>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <MDBRipple
+              className='bg-image hover-overlay shadow-1-strong rounded'
+              rippleTag='div'
+              rippleColor='light'
+            >
+              <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp' width={400} height={400} className="card-img" alt="..." />
+              </a>
+            </MDBRipple>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <MDBRipple
+              className='bg-image hover-overlay shadow-1-strong rounded'
+              rippleTag='div'
+              rippleColor='light'
+            >
+              <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp' width={400} height={400} className="card-img" alt="..." />
+              </a>
+            </MDBRipple>
+            <div class="card-body pb -6">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <div class="col">
+          <div class="card">
+            <MDBRipple
+              className='bg-image hover-overlay shadow-1-strong rounded'
+              rippleTag='div'
+              rippleColor='light'
+            >
+              <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp' width={400} height={400} className="card-img" alt="..." />
+              </a>
+            </MDBRipple>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card">
+            <MDBRipple
+              className='bg-image hover-overlay shadow-1-strong rounded'
+              rippleTag='div'
+              rippleColor='light'
+            >
+              <a href='https://github.com/hcruz77/tech-blog' target="_blank" rel="noreferrer"><img src='https://mdbootstrap.com/img/new/fluid/city/113.webp' width={400} height={400} className="card-img" alt="..." />
+              </a>
+            </MDBRipple>
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-    </section>
-  </div>
-  )
-  
+
+
+  );
 };
 
 export default Portfolio;
