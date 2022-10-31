eafb9e3bba477dce1d311e3aed168155c64bed32
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-10-30 22:16:40 -0400

    added text to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,11 +4,22 @@ const Resume = () => {
   return (
     <div className="container-fluid p-5 min-vh-100">
       <h1>Resume</h1>
-      <div>
-      <a href="https://drive.google.com/file/d/1pA-YevPnxam4XHRXJh7vzMmT9OOhFNlS/view?usp=sharing"target="_blank" rel="noreferrer">My Resume
+      <div className="my-3">
+      <a href="https://drive.google.com/file/d/1pA-YevPnxam4XHRXJh7vzMmT9OOhFNlS/view"target="_blank" rel="noreferrer" className="fs-5" >My Resume
       </a>
     </div>
+    <div className="my-5">
+      <h3>Front-End Proficiencies</h3>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
     <div>
+      <h3>Back-End Proficiencies</h3>
       <ul>
         <li></li>
         <li></li>
