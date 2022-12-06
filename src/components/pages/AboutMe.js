4a556bf6625ed88ae5a556257625c76cdc8a292e
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-12-06 16:53:31 -0500

    added and styled card to about me page with profile image

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,27 +1,29 @@
 import React from "react";
 import background from "../../assets/images/butterflyBackground.png"
-// import profile from "../../assets/images/profilepic.jpg"
+import profile from "../../assets/images/profilepic.jpg"
 
 const AboutMe = () => {
   return (
     <div style={{
       backgroundImage: `url(${background})`,
       backgroundSize: 'cover'
-    }} >
+      }} >
       <div className="container-fluid p-5 min-vh-100">
-        <div className="row col-5">
-          <h1 className="fw-bold pt-3">About Me</h1>
-          <p>
-            Talulah always saw the world in black and white. There were two choices for every situation and you had to choose one of them. It was therefore terribly uncomfortable for her to spend time with Frank. He saw the world in shades of gray with hundreds of choices to choose from in every situation.
-            The trees, therefore, must be such old and primitive techniques that they thought nothing of them, deeming them so inconsequential that even savages like us would know of them and not be suspicious. At that, they probably didn't have too much time after they detected us orbiting and intending to land. And if that were true, there could be only one place where their civilization was hidden.
-            {/* paragraphs taken from randomwordgenerator.com */}
-          </p>
-          {/* <div className="row row-cols-2">
-            <div className="col-4">
-              <img src={profile} class="img-thumbnail" alt="..." />
-            </div>
-          </div> */}
+        {/* <div className="row col-8"> */}
+        <h1 className="fw-bold pt-3 ">About Me</h1>
 
+        <div class="card mb-3 bg-transparent text-dark border border-gray">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src={profile} class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer kjhkjh klhlkjhljkhkjhl kljhlkjhl kjh klh . This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer kjhkjh klhlkjhljkhkjhl kljhlkjhl kjh klh </p>
+
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div >
