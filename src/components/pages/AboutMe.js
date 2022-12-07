2f6eca7b850653eec7485d0bcd698449e64271d3
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-12-07 18:27:39 -0500

    added project images, black and white hover styling

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -11,14 +11,14 @@ const AboutMe = () => {
       <div className="container-fluid p-5 min-vh-100">
         <h1 className="fw-bold pt-3 ">About Me</h1>
 
-        <div class="card mb-3 p-3 bg-transparent text-dark border border-gray">
-          <div class="row g-0">
-            <div class="col-md-4">
-              <img src={profile} class="img-fluid rounded" alt="..." />
+        <div className="card mb-3 p-3 bg-transparent text-dark border border-gray">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={profile} className="img-fluid rounded" alt="..." />
             </div>
-            <div class="col-md-8">
-              <div class="card-body">
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer kjhkjh klhlkjhljkhkjhl kljhlkjhl kjh klh . This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer kjhkjh klhlkjhljkhkjhl kljhlkjhl kjh klh </p>
+            <div className="col-md-8">
+              <div className="card-body">
+                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer kjhkjh klhlkjhljkhkjhl kljhlkjhl kjh klh . This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer kjhkjh klhlkjhljkhkjhl kljhlkjhl kjh klh </p>
 
               </div>
             </div>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,83 +1,83 @@
 import React, { useState } from "react";
-import butterflyOne from "../../assets/images/butterflyOne.jpg";
-import butterflyTwo from "../../assets/images/butterflyTwo.jpg";
-import butterflyThree from "../../assets/images/butterflyThree.jpg";
-import butterflyFour from "../../assets/images/butterflyFour.jpg";
-import butterflyFive from "../../assets/images/butterflyFive.jpg";
-import butterflySeven from "../../assets/images/butterflySeven.jpg";
+import bullies from "../../assets/images/bullies.png";
+import techBlog from "../../assets/images/techBlog.png";
+import passwordGen from "../../assets/images/passwordGen.png";
+import weatherDash from "../../assets/images/weatherDash.png";
+import eComBackend from "../../assets/images/eComBackend.png";
+import codeQuiz from "../../assets/images/codeQuiz.png";
 import background from "../../assets/images/butterflyBackground.png"
 
 const Portfolio = () => {
   const [projects] = useState([
     {
       id: 1,
-      title: "Text Editor",
-      image: butterflyOne,
-      link: "https://github.com/hcruz77/text-editor",
-      description: "PWA"
+      title: "Bullies To The Rescue",
+      image: bullies,
+      link: "https://github.com/hcruz77/bullies-to-the-rescue",
+      description: "MERN"
     },
 
     {
       id: 2,
       title: "Tech Blog",
-      image: butterflyTwo,
+      image: techBlog,
       link: "https://github.com/hcruz77/tech-blog",
       description: "Full Stack MVC"
     },
 
     {
       id: 3,
-      title: "Note Taker",
-      image: butterflyThree,
-      link: "https://github.com/hcruz77/note-taker",
+      title: "Password Generator",
+      image: passwordGen,
+      link: "https://github.com/hcruz77/password-generator",
       description: "JSON Server"
     },
 
     {
       id: 4,
       title: "Weather Dashboard",
-      image: butterflyFour,
+      image: weatherDash,
       link: "https://github.com/hcruz77/weather-dashboard",
       description: "Server-Side APIs"
     },
 
     {
       id: 5,
-      title: "Work Day Scheduler",
-      image: butterflyFive,
-      link: "https://github.com/hcruz77/work-day-scheduler",
+      title: "E-Commerce Back End",
+      image: eComBackend,
+      link: "https://github.com/hcruz77/e-commerce-back-end",
       description: "jQuery/Bootstrap"
     },
 
     {
       id: 6,
-      title: "Password Generator",
-      image: butterflySeven,
-      link: "https://github.com/hcruz77/password-generator",
-      description: "JavaScript"
+      title: "Code Quiz",
+      image: codeQuiz,
+      link: "https://github.com/hcruz77/code-quiz",
+      description: ""
     },
   ]);
 
   return (
     <div style={{
-      backgroundImage: `url(${background})`, backgroundSize: 'cover' }} >
+      backgroundImage: `url(${background})`, backgroundSize: 'cover'
+    }} >
       <div className=" p-5">
         <h1 className="fw-bold pt-3">Portfolio</h1>
         <div className="container-fluid p-5 min-vh-100">
           <div className="row row-cols-1 row-cols-md-3 g-4">
             {projects.map((project) => (
               <div key={project.id}>
-                <div className="col">
-                  <div className="card border border-dark shadow p-3 mb-5 bg-body rounded">
-                    <img
-                      src={project.image}
-                      className="card-img-top"
-                      alt="..."
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title"><a href={project.link} target="_blank" rel="noreferrer">{project.title}</a></h5>
-                      <p className="card-text">{project.description}</p>
-                    </div>
+
+                <div className="card h-100 border border-dark shadow p-3 mb-5 bg-body rounded bg-transparent">
+                  <img
+                    src={project.image}
+                    className="card-img-top gray-clr"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title"><a href={project.link} target="_blank" rel="noreferrer">{project.title}</a></h5>
+                    <p className="card-text">{project.description}</p>
                   </div>
                 </div>
               </div>
